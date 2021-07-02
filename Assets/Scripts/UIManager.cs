128e582199acbe79982ec224d975e38641d7b91d
mars16-exe <83722047+mars16-exe@users.noreply.github.com>
2021-07-02 17:44:25 +0600

    13th Commit
    -Added mainmenu
    -restart, start, quit buttons added.
    -fixed bugs while scene transition

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class UIManager : Singleton<UIManager>
 {
@@ -10,9 +12,28 @@ public class UIManager : Singleton<UIManager>
     public Text score;
     public Text waveNumber;
 
+    public Text gameoverScore;
+    public Text gameoverWave;
+
+    public Button restartButton;
+    public Button menuButton;
+
+    public GameObject scorePanel;
+    public GameObject gameOverPanel;
+
     private int health = 100;
     private int currentScore;
     private int currentWave;
+
+    public delegate void onGameOver();
+    public static event onGameOver OnGameOver;
+
+    private void Start()
+    {
+        scorePanel.SetActive(true);
+        gameOverPanel.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
 
     public void UpdateScore(int newScore)
     {
@@ -29,13 +50,45 @@ public class UIManager : Singleton<UIManager>
         health -= damage;
     }
 
+    public void GameOverScreen()
+    {
+        scorePanel.SetActive(false);
+        gameOverPanel.SetActive(true);
+
+        gameoverScore.text = "Score: " + currentScore;
+        gameoverWave.text = "Wave: " + currentWave;
+
+        Time.timeScale = 0f;
+        
+    }
+
+    public void RestartLevel()
+    {
+        if(Time.timeScale != 1.0f)
+        { 
+            Time.timeScale = 1.0f;
+        }
+
+        SceneManager.LoadScene(1);
+        OnGameOver.Invoke();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     private void Update()
     {
         healthBar.value = health;
 
         score.text = "Score: " + currentScore;
         waveNumber.text = "Wave: " + currentWave;
-    }
 
+        if(health == 0)
+        {
+            GameOverScreen();
+        }
+    }
 
 }
